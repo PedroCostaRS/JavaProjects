e197bd8c76dfe4995e1529c9f9613dc1d11ea2a4
PedroCostaRS <tecpedrocosta@gmail.com>
2022-10-29 23:38:44 -0300

    Implementando a Class Categoria

---
+++ cursomc/src/main/java/com/sockel/cursomc/domain/Categoria.java
@@ -0,0 +1,70 @@
+package com.sockel.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// Atributos da classe devem ser privados para serem acessados através dos
+	// getter e setters
+	private Integer id;
+	private String nome;
+
+	// Getter do ID
+	public Integer getId() {
+		return id;
+	}
+
+	// Setter do ID
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	// Getter do Nome
+	public String getNome() {
+		return nome;
+	}
+
+	// Setter do Nome
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// Construtor vazio, pode ser instanciado sem atribuir dados aos atributos
+	public Categoria() {
+
+	}
+
+	// Gerado automaticamente (Botão direito, source, Generate Constructor Usin
+	// Fields)
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	// Gerado para comparar um atributo por valor e não posição na memória
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	// Gerado para comparar um atributo por valor e não posição na memória
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+	
+	
+}
--- cursomc/src/main/java/com/sockel/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/sockel/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package com.sockel.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sockel.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest está funcionando normalmente";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 	
