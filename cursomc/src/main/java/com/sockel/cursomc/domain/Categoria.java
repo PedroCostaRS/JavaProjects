4bd13d0013c5596f754ab431d2c8f5b36d154697
PedroCostaRS <tecpedrocosta@gmail.com>
2022-10-30 22:55:26 -0300

    Implementando primeira tabela

--- cursomc/src/main/java/com/sockel/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/sockel/cursomc/domain/Categoria.java
@@ -3,11 +3,19 @@ package com.sockel.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	// Atributos da classe devem ser privados para serem acessados através dos
 	// getter e setters
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
