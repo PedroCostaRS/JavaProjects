cedff7cd62e7ec22ea94d27cbad49df5042a5909
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-04 20:17:46 -0300

    Curso java do básico ao avancado

--- cursomc/src/main/java/com/sockel/cursomc/resources/exception/ResourceExceptionHandler.java
+++ cursomc/src/main/java/com/sockel/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 import com.sockel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,7 +15,7 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
-	StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-}
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
 }
