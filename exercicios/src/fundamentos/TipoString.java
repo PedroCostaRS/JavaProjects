03364840da60f8e1b59690e885cec896c6e688e3
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-05 00:29:31 -0300

    Curso java do básico ao avancado aula 6 - String

---
+++ exercicios/src/fundamentos/TipoString.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+
+		// Mostra o índice 2 da palavra e-ou variável
+		System.out.println("Teste".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		
+		
+	}
+
+}
