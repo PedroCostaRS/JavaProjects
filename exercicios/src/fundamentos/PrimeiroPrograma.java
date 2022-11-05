061834b7328a0024259a82b3ab444d14f3585d88
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-04 22:51:40 -0300

    Curso java do básico ao avancado aula 2

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,11 +1,12 @@
 package fundamentos;
 
 public class PrimeiroPrograma {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Aula 1");
-		
+		System.out.println("Aula 1; Parte 2");
+
 	}
 
 }
