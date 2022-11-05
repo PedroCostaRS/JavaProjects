0dcd437803a24bac73474c614df33936deaddde6
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-05 16:38:18 -0300

    Curso java do básico ao avancado aula 6 - String

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -8,7 +8,12 @@ public class TipoString {
 		System.out.println("Teste".charAt(2));
 		
 		String s = "Boa tarde";
-		System.out.println(s.concat("!!!"));
+		System.out.println(s.concat("!!!")); // função para concatenar
+		System.out.println(s.startsWith("Boa")); // Testa se a string inicia com a palavra boa.
+		System.out.println(s.toLowerCase()); // Exibe tudo minúsculo
+		System.out.println(s.toUpperCase()); // Exibe tudo maiúsculo
+		System.out.println(s.length()); // retorna quantidade de caracteres da string
+		
 		
 		
 	}
