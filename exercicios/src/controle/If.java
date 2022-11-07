867c39c806d26ceaec61b6f3de2f55acb83c90f0
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-06 21:47:45 -0300

    Curso java do básico ao avancado aula 9 - Estrutura de controle

--- exercicios/src/controle/If.java
+++ exercicios/src/controle/If.java
@@ -1,10 +1,27 @@
 package controle;
 
+import java.util.Scanner;
+
 public class If {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Informe a média: ");
+		double media = entrada.nextDouble();
+
+		entrada.close();
+
+		if (media < 6) {
+
+			System.out.println("Reprovado!");
+
+		} else if (media >= 6 && media < 9.9) {
+			System.out.println("Aprovado!!");
+		} else {
+			System.out.println("Aprovado com louvor. Aluno nota 10");
+		}
+
 	}
 }
