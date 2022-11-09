db7756583cc6c2d1004b469885cada2e06e90036
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-09 13:53:38 -0300

    curso java do básico ao avançado aula 14 - metodos

---
+++ exercicios/src/classes/ProdutoTeste.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class ProdutoTeste {
+
+	public static void main(String[] args) {
+
+		Produto p1 = new Produto();
+		p1.nome = "Notebook";
+		p1.preco = 4356.89;
+		p1.desconto = 0.25;
+		
+		
+
+	}
+
+}
